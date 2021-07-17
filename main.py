c6189395ce44795cbb525c17617578b4c505e159
iamyajat <myajat21@gmail.com>
2021-07-17 20:17:44 +0530

    refact: renamed folder

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from src.box_extractor import box_extraction
-from src.character_predictor import predict
-from src.spelling_corrector import fix_spellings
-from src.text_similarity import check_similarity, get_marks
+from auto_grader.box_extractor import box_extraction
+from auto_grader.character_predictor import predict
+from auto_grader.spelling_corrector import fix_spellings
+from auto_grader.text_similarity import check_similarity, get_marks
 
 box_extraction("./samples/form.jpg", "./samples/output/")
 
