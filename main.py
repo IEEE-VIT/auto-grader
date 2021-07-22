638bda2266731391612a0e1414d96e9c09bf0b48
anshul_ubuntu <ans29hul@gmail.com>
2021-07-22 23:18:53 +0530

    update

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from autograder.character_predictor import predict
 # from autograder.spelling_corrector import fix_spellings
 # from autograder.text_similarity import check_similarity, get_marks
 
-characters, coordinates = box_extraction(
-    "./samples/form_scanned_4.jpg", "./samples/output/"
+charactersAll, coordinates = box_extraction(
+    "./samples/form_scanned_2.jpg", "./samples/output/"
 )
 
-if len(characters) == 380:
+# print(coordinates)
+
+print(len(charactersAll))
+
+if len(charactersAll) == 380:
 
     # def csort(e):
     #     return 1000 * e[0] + e[1]
@@ -17,33 +21,36 @@ if len(characters) == 380:
     # for c in coordinates:
     #     print(c)
 
+    locations = []
     prev = 0
     sentence = ""
-    for i in range(len(characters)):  # boxes
-        pix = 0
-        for j in range(len(characters[i])):  # pixels
-            debug = characters[i][j]
-            if characters[i][j] != 0:
-                pix = pix + 1
-        if pix < 200:
-            print("A  ", len(characters[prev:i]))
-            if len(characters[prev:i]) != 0:
-                sentence += "".join(predict(characters[prev:i])) + " "
-                # print(predict(characters[prev:i]))
-            prev = i
+    print(charactersAll.shape)
+    for n in range(10):
+        characters = charactersAll[38 * n : 38 * (n + 1)]
+        for i in range(len(characters)):  # boxes
+            pix = 0
+            for j in range(len(characters[i])):  # pixels
+                debug = characters[i][j]
+                if characters[i][j] != 0:
+                    pix = pix + 1
+            if pix < 30:
+                subarray = characters[prev:i].reshape(-1, 28, 28, 1)
+                if subarray.shape[0] > 1:
+                    # print(subarray.shapes)
+                    try:
+                        pred = predict(subarray)
+                        sentence += "".join(pred) + " "
+                    except:
+                        print(subarray.shape)
+                prev = i + 1
 
-    print(sentence)
+        print(sentence)
 
-    # x = predict(characters)
+        # query = fix_spellings(sentence)
 
-    # for i in range(len(x)):
-    #     print(i + 1, x[i], sep=": ")
+        # cos_scores = check_similarity(["sky is blue"], query)
 
-    # query = fix_spellings("a blu4 sky")
+        # print(get_marks(cos_scores, 5, (0.45, 0.85)))
 
-    # cos_scores = check_similarity(["sky is blue"], query)
-
-    # print(get_marks(cos_scores, 5, (0.45, 0.85)))
-
-    # [Y],[A],[J],[ ],[ ],[ ],[ ],[ ],[ ],[A],[T]
-    # YAJ AT
+        # [Y],[A],[J],[ ],[ ],[ ],[ ],[ ],[ ],[A],[T]
+        # YAJ AT
