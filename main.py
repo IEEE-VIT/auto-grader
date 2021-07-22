2545bf7baf1a698ccd7b76d002f791dd1293be88
anshul_ubuntu <ans29hul@gmail.com>
2021-07-22 19:33:21 +0530

    fix: minor changes

--- main.py
+++ main.py
@@ -8,30 +8,42 @@ characters, coordinates = box_extraction(
     "./samples/form_scanned_4.jpg", "./samples/output/"
 )
 
+if len(characters) == 380:
 
-def csort(e):
-    return 1000 * e[0] + e[1]
+    # def csort(e):
+    #     return 1000 * e[0] + e[1]
 
+    # coordinates.sort(key=csort)
+    # for c in coordinates:
+    #     print(c)
 
-coordinates.sort(key=csort)
-print(coordinates)
+    prev = 0
+    sentence = ""
+    for i in range(len(characters)):  # boxes
+        pix = 0
+        for j in range(len(characters[i])):  # pixels
+            debug = characters[i][j]
+            if characters[i][j] != 0:
+                pix = pix + 1
+        if pix < 200:
+            print("A  ", len(characters[prev:i]))
+            if len(characters[prev:i]) != 0:
+                sentence += "".join(predict(characters[prev:i])) + " "
+                # print(predict(characters[prev:i]))
+            prev = i
 
-pix = 0
-for i in range(len(characters)):
-    for j in range(len(characters[i])):
-        if j != 0:
-            pix = pix + 1
-    if pix < 20:
-        characters.drop(i)
+    print(sentence)
 
+    # x = predict(characters)
 
-x = predict(characters)
+    # for i in range(len(x)):
+    #     print(i + 1, x[i], sep=": ")
 
-for i in range(len(x)):
-    print(i + 1, x[i], sep=": ")
+    # query = fix_spellings("a blu4 sky")
 
-# query = fix_spellings("a blu4 sky")
+    # cos_scores = check_similarity(["sky is blue"], query)
 
-# cos_scores = check_similarity(["sky is blue"], query)
+    # print(get_marks(cos_scores, 5, (0.45, 0.85)))
 
-# print(get_marks(cos_scores, 5, (0.45, 0.85)))
+    # [Y],[A],[J],[ ],[ ],[ ],[ ],[ ],[ ],[A],[T]
+    # YAJ AT
